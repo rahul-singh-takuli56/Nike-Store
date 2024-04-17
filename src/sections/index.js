275a077f7bab6fca9ff3ddb2e38421b00cd53150
Rahul Singh Takuli <118590603+rahul-singh-takuli56@users.noreply.github.com>
2024-04-17 18:20:13 +0530

    Tailwind CSS Project

---
+++ src/sections/index.js
@@ -0,0 +1,12 @@
+import Hero from "./Hero"
+import Footer from "./Footer"
+import CustomerReviews from "./CustomerReviews"
+import PopularProducts from "./PopularProducts"
+import SpecialOffer from "./SpecialOffer"
+import Subscribe from "./Subscribe"
+import Services from "./Services"
+import SuperQuality from "./SuperQuality"
+
+export {
+    Hero, Footer, SuperQuality, CustomerReviews, PopularProducts, SpecialOffer, Services, Subscribe
+}
